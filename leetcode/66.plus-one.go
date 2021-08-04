b34f9e08f2650dcff49ed00d1b35ec6bc6e384e9
Youhan Wu <youhan.wu@outlook.com>
2021-08-04 23:38:22 +0800

    2021/08/04

---
+++ leetcode/66.plus-one.go
@@ -0,0 +1,30 @@
+package mian
+
+/*
+ * @lc app=leetcode id=66 lang=golang
+ *
+ * [66] Plus One
+ */
+
+// @lc code=start
+func plusOne(digits []int) []int {
+	index := len(digits) - 1
+	if index < 0 {
+		return digits
+	}
+	for ; index >= 0; index-- {
+		if digits[index] != 9 {
+			break
+		} else {
+			digits[index] = 0
+		}
+	}
+	if index < 0 {
+		digits = append([]int{1}, digits...)
+	} else {
+		digits[index] = digits[index] + 1
+	}
+	return digits
+}
+
+// @lc code=end
