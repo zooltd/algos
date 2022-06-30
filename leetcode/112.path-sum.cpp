51430be907561222fb5ec14ed92be4b5f6ca7395
Youhan Wu <youhan.wu@outlook.com>
2022-06-30 00:13:41 -0500

    2022/06/29

--- leetcode/112.path-sum.cpp
+++ leetcode/112.path-sum.cpp
@@ -21,7 +21,7 @@ public:
     bool hasPathSum(TreeNode* root, int targetSum) {
         if (!root) return false;
         targetSum -= root->val;
-        if (targetSum == 0 && !root->left && !root->right) return true;
+        if (!root->left && !root->right) return targetSum == 0;
         return hasPathSum(root->left, targetSum) || hasPathSum(root->right, targetSum);
     }
 };
