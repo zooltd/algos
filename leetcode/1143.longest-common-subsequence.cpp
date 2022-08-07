9f9a9821f30eba23e594bc383626108b44c5af74
Youhan Wu <youhan.wu@outlook.com>
2022-08-06 22:04:24 -0500

    2022/08/06

--- leetcode/1143.longest-common-subsequence.cpp
+++ leetcode/1143.longest-common-subsequence.cpp
@@ -1,4 +1,5 @@
-#include <bits/stdc++.h>
+#include <string>
+#include <vector>
 using namespace std;
 
 /*
@@ -10,21 +11,21 @@ using namespace std;
 // @lc code=start
 class Solution {
 public:
-  int longestCommonSubsequence(string text1, string text2) {
-    int n = text1.length();
-    int m = text2.length();
-    text1 = " " + text1;
-    text2 = " " + text2;
-    vector<vector<int>> f(n + 1, vector<int>(m + 1, 0));
-    for (int i = 1; i <= n; i++) {
-      for (int j = 1; j <= m; j++) {
-        if (text1[i] == text2[j])
-          f[i][j] = f[i - 1][j - 1] + 1;
-        else
-          f[i][j] = max(f[i - 1][j], f[i][j - 1]);
-      }
+    int longestCommonSubsequence(string text1, string text2) {
+        int n = text1.length();
+        int m = text2.length();
+        text1 = " " + text1;
+        text2 = " " + text2;
+        vector<vector<int>> f(n + 1, vector<int>(m + 1, 0));
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+              if (text1[i] == text2[j])
+                f[i][j] = f[i - 1][j - 1] + 1;
+              else
+                f[i][j] = max(f[i - 1][j], f[i][j - 1]);
+            }
+        }
+        return f[n][m];
     }
-    return f[n][m];
-  }
 };
 // @lc code=end
--- leetcode/72.edit-distance.cpp
+++ leetcode/72.edit-distance.cpp
@@ -1,3 +1,7 @@
+#include <vector>
+#include <algorithm>
+#include <string>
+using namespace std;
 /*
  * @lc app=leetcode id=72 lang=cpp
  *
@@ -7,26 +11,24 @@
 // @lc code=start
 class Solution {
 public:
-  int minDistance(string word1, string word2) {
-    int n = word1.size();
-    int m = word2.size();
-    word1 = " " + word1;
-    word2 = " " + word2;
-    /**
-     * f[i,j]: # steps to transform first i characters of `word1`
-     * to first j characters of `word2`
-     */
-    vector<vector<int>> f(n + 1, vector<int>(m + 1, 0));
-    for (int i = 0; i <= n; i++)
-      f[i][0] = i; // abc->[]
-    for (int j = 0; j <= m; j++)
-      f[0][j] = j; //[]->abc
-    for (int i = 1; i <= n; i++)
-      for (int j = 1; j <= m; j++)
-        f[i][j] = min(min(f[i - 1][j] + 1, f[i][j - 1] + 1),
-                      f[i - 1][j - 1] + (word1[i] != word2[j]));
+    int minDistance(string word1, string word2) {
+        int n = word1.size();
+        int m = word2.size();
+        word1 = " " + word1;
+        word2 = " " + word2;
+        /**
+        * f[i,j]: # steps to transform word1[1~i] to word2[1~j]
+        */
+        vector<vector<int>> f(n + 1, vector<int>(m + 1, 0));
+        for (int i = 0; i <= n; i++)
+            f[i][0] = i; // abc->[]
+        for (int j = 0; j <= m; j++)
+            f[0][j] = j; //[]->abc
+        for (int i = 1; i <= n; i++)
+            for (int j = 1; j <= m; j++)
+              f[i][j] = min({f[i - 1][j] + 1, f[i][j - 1] + 1, f[i - 1][j - 1] + (word1[i] != word2[j])});
 
-    return f[n][m];
-  }
+        return f[n][m];
+    }
 };
 // @lc code=end
